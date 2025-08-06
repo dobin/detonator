89fa52f87a4ad1aed13c8663ca9448e312edd850
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-08-06 07:34:13 +0200

    refactor: comment in proxmox permissions

--- scripts/proxmox/proxmox_permissions.sh
+++ scripts/proxmox/proxmox_permissions.sh
@@ -3,8 +3,12 @@
 # Add minimal permissions
 pveum role add VM.ControlLimited -privs "VM.PowerMgmt VM.Snapshot.Rollback VM.Audit"
 
-# Give user rededr@pve the new role on a VM
+# Give USER rededr@pve the new role on a VM
 pveum aclmod /vms/101 --user rededr@pve --role VM.ControlLimited
+
+# or, Give TOKERN rededr@pve!detonator the new role on a VM
+# tokens are prefered as they dont expire
+pveum aclmod /vms/101 --token 'rededr@pve!detonator' --role VM.ControlLimited
 
 # Check
 pveum user permissions rededr@pve
