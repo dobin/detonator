2690e9461c904f9c0582e09e4fddd9b18fd5a347
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-12-26 12:16:23 +0100

    fix: tests

--- tests/test_parser_defender.py
+++ tests/test_parser_defender.py
@@ -1,15 +1,30 @@
 import logging
 import unittest
+from typing import Optional, Dict, List, Type
+import datetime
+
 from detonatorapi.edr_parser.parser_defender import DefenderParser
+from detonatorapi.settings import UPLOAD_DIR, AGENT_DATA_GATHER_INTERVAL
+from detonatorapi.database import Submission, SubmissionAlert, get_db_direct
+from detonatorapi.edr_parser.edr_parser_manager import EdrParser, get_relevant_edr_parser
 
 
 class TestParserDefender(unittest.TestCase):
     def test_parser_defender(self):
         with open("tests/data/defender_result.xml", "r") as file:
             edr_telemetry_raw = file.read()
-        parser = DefenderParser()
-        parser.load(edr_telemetry_raw)
-        self.assertTrue(parser.is_relevant(), "Parser should be relevant for Defender logs")
-        parser.parse()
-        self.assertEqual(len(parser.events), 7, "Expected one event in the parsed data")
-        self.assertEqual(parser.events[0]['category_name'], "Suspicious Behaviour", "Expected category name to be 'Malware'")
+
+        edr_parser: Optional[Type[EdrParser]] = get_relevant_edr_parser(edr_telemetry_raw)
+        self.assertIsNotNone(edr_parser, "DefenderParser should be recognized as relevant parser.")
+        self.assertEqual(edr_parser, DefenderParser, "The relevant parser should be DefenderParser.")
+
+        ok, alerts, is_detected = edr_parser.parse(edr_telemetry_raw)
+        self.assertTrue(ok, "Parsing should complete successfully.")
+        self.assertTrue(is_detected, "A detection should be found in the telemetry.")
+        self.assertGreater(len(alerts), 0, "At least one alert should be generated.")
+        first_alert: SubmissionAlert = alerts[0]
+        self.assertEqual(first_alert.alert_id, "{6029D8AE-A305-41BD-9BF4-4378168C91C0}", "Alert ID should match expected value.")
+        self.assertEqual(first_alert.title, "Behavior:Win32/Meterpreter.gen!A", "Alert title should match expected value.")
+        self.assertEqual(first_alert.severity, "Severe", "Alert severity should match expected value.")
+        self.assertEqual(first_alert.category, "Suspicious Behaviour", "Alert category should match expected value.")
+        self.assertEqual(first_alert.detected_at, datetime.datetime(2025, 7, 12, 18, 45, 56, 925000, tzinfo=datetime.timezone.utc), "Alert detected_at should match expected value.") 
